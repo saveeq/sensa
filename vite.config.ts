48644cb1f622c024d54db3ebf801b6f339f84c48
SaveliyLap <saveeeqx@gmail.com>
2026-03-04 16:14:34 +0300

    transit

--- vite.config.ts
+++ vite.config.ts
@@ -19,6 +19,13 @@ export default defineConfig({
         target: "http://127.0.0.1:3001",
         changeOrigin: true,
         secure: false,
+
+      },
+      "/ws": {
+        target: "ws://127.0.0.1:4000",
+        ws: true,
+        changeOrigin: true,
+        rewriteWsOrigin: true,
       },
     },
   },
